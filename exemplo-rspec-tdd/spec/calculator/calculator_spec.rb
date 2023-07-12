aa0c2c0e90ac2d4dec062789d81f4cc1b16e25c7
Sergio Oseko <osk.sergio@gmail.com>
2023-07-12 17:39:14 -0300

    test: matchers de erro (fim da aula #22)

--- exemplo-rspec-tdd/spec/calculator/calculator_spec.rb
+++ exemplo-rspec-tdd/spec/calculator/calculator_spec.rb
@@ -5,6 +5,9 @@ describe Calculator do
   context '#div' do
     it 'divide by 0' do
       expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError)
+      expect{subject.div(3, 0)}.to raise_error('divided by 0')
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError, 'divided by 0')
+      expect{subject.div(3, 0)}.to raise_error(/divided/)
     end
   end
 
