b67b8cd8502721db2f2690b934c92b5928e9bee6
Sergio Oseko <osk.sergio@gmail.com>
2023-07-20 14:53:54 -0300

    test: one-liner syntax (aula #26)

--- exemplo-rspec-tdd/spec/matchers/ranges/ranges_spec.rb
+++ exemplo-rspec-tdd/spec/matchers/ranges/ranges_spec.rb
@@ -4,4 +4,12 @@ describe (1..5), 'Ranges' do
 		expect(subject).to cover(3,5)
 		expect(subject).not_to cover(0,6)
 	end
+
+	# aula 26:
+	# abaixo estão exemplos de códigos usando o "one-liner syntax"
+	it { is_expected.to cover(2) }
+	it { is_expected.to cover(3,5) }
+	it { is_expected.not_to cover(0,6) }
+
+	it { expect(subject).to cover(2) }
 end
