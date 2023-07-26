5c5809dfd9f1f40ca2b32541f391d032d81a9c53
Sergio Oseko <osk.sergio@gmail.com>
2023-07-26 06:31:26 -0300

    test: helper methods (arbitrário e módulo)

--- exemplo-rspec-tdd/spec/composicao/composicao_spec.rb
+++ exemplo-rspec-tdd/spec/composicao/composicao_spec.rb
@@ -2,7 +2,11 @@ describe 'Ruby on Rails' do
   it { is_expected.to start_with('Ruby').and end_with('Rails') }
   it { expect(fruta).to eq('banana').or eq('laranja').or eq('uva') }
 
-  def fruta
-    %w(banana laranja uva).sample
-  end
+  # Podemos usar os helpers de módulo p/ não poluir os testes
+  # ver pasta "exemplo-rspec-tdd/helpers"
+  #
+  # Estes helper seria arbitrário (dentro do teste)
+  #def fruta
+  #  %w(banana laranja uva).sample
+  #end
 end
