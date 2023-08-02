61b70660c3fb8ab589e05cc802996073fce35a35
Sergio Oseko <osk.sergio@gmail.com>
2023-08-02 06:27:22 -0300

    test: finalizada a aula #34 sobre o let

--- exemplo-rspec-tdd/spec/matchers/abributos/atributos_let_spec.rb
+++ exemplo-rspec-tdd/spec/matchers/abributos/atributos_let_spec.rb
@@ -1,21 +1,18 @@
 require 'pessoa'
 
 describe 'Atributos' do
-  before(:each) do
-    @pessoa = Pessoa.new
-  end
+
+  let(:pessoa) { Pessoa.new }
 
   it 'have_attributes 1' do
-    @pessoa.nome = 'Sergio'
-    @pessoa.idade = 45
-
-    expect(@pessoa).to have_attributes(nome: a_string_starting_with('S'), idade: (be >= 20))
+    pessoa.nome = 'Sergio'
+    pessoa.idade = 45
+    expect(pessoa).to have_attributes(nome: a_string_starting_with('S'), idade: (be >= 20))
   end
 
   it 'have_attributes 2' do
-    @pessoa.nome = 'Chipilique'
-    @pessoa.idade = 25
-
-    expect(@pessoa).to have_attributes(nome: a_string_starting_with('C'), idade: (be >= 20))
+    pessoa.nome = 'Chipilique'
+    pessoa.idade = 25
+    expect(pessoa).to have_attributes(nome: a_string_starting_with('C'), idade: (be >= 20))
   end
 end
