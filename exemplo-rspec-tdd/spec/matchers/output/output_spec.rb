d7b08e2065d32f4815e358f13b980e377df1ade8
Sergio Oseko <osk.sergio@gmail.com>
2023-08-07 06:19:13 -0300

    test: finalizada a aula #36 matcher output

--- exemplo-rspec-tdd/spec/matchers/output/output_spec.rb
+++ exemplo-rspec-tdd/spec/matchers/output/output_spec.rb
@@ -1,3 +1,9 @@
 describe 'Matcher output' do
-  it { expect(puts "sergio").to output.to_stdout }
+  it { expect{ puts "sergio" }.to output.to_stdout }
+  it { expect{ print "sergio" }.to output("sergio").to_stdout }
+  it { expect{ puts "sergio oseko" }.to output(/sergio/).to_stdout }
+
+  it { expect{ warn "sergio" }.to output.to_stderr }
+  it { expect{ warn "sergio" }.to output("sergio\n").to_stderr }
+  it { expect{ warn "sergio oseko" }.to output(/sergio/).to_stderr }
 end
