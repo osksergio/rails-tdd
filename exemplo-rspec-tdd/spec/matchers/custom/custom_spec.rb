8232e3376c21fb58abde0078f796108a6c7f6988
Sergio Oseko <osk.sergio@gmail.com>
2023-08-15 06:28:25 -0300

    test: continuando a aula #40 (customizando matchers)

--- exemplo-rspec-tdd/spec/matchers/custom/custom_spec.rb
+++ exemplo-rspec-tdd/spec/matchers/custom/custom_spec.rb
@@ -1,5 +1,12 @@
 RSpec::Matchers.define :be_a_multiple_of do |expected|
+
+  # expected == 3
+  # actual == subject == 18
   match do |actual|
     actual % expected == 0
   end
+end
+
+describe 18, 'Custom Matcher' do
+  it { is_expected.to be_a_multiple_of(3) }
 end
