1c5f2c206d9f02774ea9a8cd025bddaa19b88882
Sergio Oseko <osk.sergio@gmail.com>
2023-08-19 20:37:13 -0300

    test: concluída a aula #42

--- exemplo-rspec-tdd/spec/test_doubles/user_spec.rb
+++ exemplo-rspec-tdd/spec/test_doubles/user_spec.rb
@@ -1,7 +1,10 @@
 describe 'Test Double' do
   it '--' do
     user = double('User')
-    allow(user).to receive_messages(name: 'Sergio', password: 'secret')
+    allow(user).to receive(:name).and_return('Sergio')
+    allow(user).to receive(:password).and_return('secret')
+    #outra forma de permitir
+    #allow(user).to receive_messages(name: 'Sergio', password: 'secret')
     user.name
     user.password
   end
