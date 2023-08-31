5bde2235f1464d09372dda815b98c6583649cc1b
Sergio Oseko <osk.sergio@gmail.com>
2023-08-31 06:02:04 -0300

    test: aula #47 - método As Null Object

--- exemplo-rspec-tdd/spec/test_doubles/user_spec.rb
+++ exemplo-rspec-tdd/spec/test_doubles/user_spec.rb
@@ -8,4 +8,13 @@ describe 'Test Double' do
     user.name
     user.password
   end
+
+  it 'as_null_object' do
+    user = double('User').as_null_object
+    allow(user).to receive(:name).and_return('Sergio')
+    allow(user).to receive(:password).and_return('secret')
+    user.name
+    user.password
+    user.email
+  end
 end
